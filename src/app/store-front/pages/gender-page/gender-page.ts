6a57db10a4b82e7a23b397e6db2a1fee294a97f1
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-04 15:35:34 -0400

    Inyectado servicio ProductService en vista GenderPage; se muestran los artículos por género

--- src/app/store-front/pages/gender-page/gender-page.ts
+++ src/app/store-front/pages/gender-page/gender-page.ts
@@ -1,9 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '@products/services/product-service';
+import { ProductCard } from '@store-front/components/product-card/product-card';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './gender-page.html',
   changeDetection: ChangeDetectionStrategy.Eager,
 })
-export class GenderPage {}
+export class GenderPage {
+
+  private activatedRoute = inject(ActivatedRoute);
+  private productService = inject(ProductService);
+
+  gender = toSignal(this.activatedRoute.params.pipe(
+    map(({gender}) => gender)
+  ))
+
+  productsResource = rxResource({
+    params: () => ({gender: this.gender()}),
+    stream: ({params}) => {
+      return this.productService.getProducts({gender: params.gender})
+    }
+  });
+}
