6c3e671c656feb4142c966135a5958b9d04316a0
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-04 14:35:30 -0400

    Creado ProductImagePipe

---
+++ src/app/products/pipes/product-image-pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, type PipeTransform } from '@angular/core';
+import { environment } from '../../../environments/environment';
+
+const baseUrl = environment.baseUrl;
+const noImage = 'assets/images/no-image.jpg';
+@Pipe({
+  name: 'productImagePipe',
+})
+export class ProductImagePipe implements PipeTransform {
+  transform(value: string | string[]): string {
+
+    switch (typeof value) {
+      case 'object': {
+        return value.length > 0 ? `${baseUrl}/files/product/${value[0]}` : `${noImage}`
+      }
+
+      case 'string': {
+        return value.length > 0 ? `${baseUrl}/files/product/${value}` : `${noImage}`
+      }
+    }
+  }
+}
