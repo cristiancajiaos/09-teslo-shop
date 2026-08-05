5aab08371975ea34741b5f4c75fbe4decbd811af
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-05 16:18:08 -0400

    Resultado de productos por vista se guarda en caché

--- src/app/products/services/product-service.ts
+++ src/app/products/services/product-service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { Product, ProductResponse } from '@products/interfaces/product.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { environment } from '../../../environments/environment.development';
 
 const baseUrl = environment.baseUrl;
@@ -16,9 +16,16 @@ export class ProductService {
 
   private http = inject(HttpClient);
 
+  private productsCache = new Map<string, ProductResponse>();
+
   getProducts(options: Options): Observable<ProductResponse> {
 
-    const {limit = 10, offset = 0, gender = ''} = options;
+    const {limit = 9, offset = 0, gender = ''} = options;
+
+    const key = `${limit}-${offset}-${gender}`; // 9-0-''
+    if (this.productsCache.has(key)) {
+      return of(this.productsCache.get(key)!);
+    }
 
     return this.http.get<ProductResponse>(`${baseUrl}/products`, {
       params: {
@@ -27,7 +34,8 @@ export class ProductService {
         gender: gender
       }
     }).pipe(
-      tap((resp) => console.log(resp))
+      tap((resp) => console.log(resp)),
+      tap((resp) => this.productsCache.set(key, resp))
     )
   }
 
