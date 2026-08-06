2910329c16927e447578d309613c476e8af724c4
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-06 10:34:20 -0400

    Creado formulario en vista LoginPage

--- src/app/auth/pages/login-page/login-page.ts
+++ src/app/auth/pages/login-page/login-page.ts
@@ -1,10 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 @Component({
   selector: 'app-login-page',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.html',
   changeDetection: ChangeDetectionStrategy.Eager,
 })
-export class LoginPage {}
+export class LoginPage {
+
+  private fb = inject(FormBuilder);
+
+  hasError = signal(false);
+  isPosting = signal(false);
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.hasError.set(true);
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
+      return;
+    }
+
+    const {email = '', password = ''} = this.loginForm.value
+
+    console.log({email, password});
+  }
+}
