d8f9e5ab9438585d00156a777c94910e1f7891f1
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-06 13:25:38 -0400

    Creado Guard NotAuthenticated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Routes } from '@angular/router';
+import { notAuthenticatedGuard } from '@auth/guards/not-authenticated-guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.routes')
-    // TODO: Guards
+    loadChildren: () => import('./auth/auth.routes'),
+    canMatch: [
+      notAuthenticatedGuard,
+      /*
+      () => {
+        console.log('hola mundo');
+        return true
+      },
+      */
+    ]
   },
   {
     path: '',
