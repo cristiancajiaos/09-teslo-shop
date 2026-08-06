318b582c612ceac4ad144315c0b5ec7d81067c13
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-06 16:39:28 -0400

    Definido Layout de componente Product Table; inyectados servicios ProductService y PaginationService a vista ProductsAdminPage

--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.ts
@@ -1,9 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ProductTable } from '../../components/product-table/product-table';
+import { ProductService } from '@products/services/product-service';
+import { PaginationService } from '@shared/components/pagination/pagination-service';
+import { Pagination } from '@shared/components/pagination/pagination';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [],
+  imports: [ProductTable, Pagination],
   templateUrl: './products-admin-page.html',
   changeDetection: ChangeDetectionStrategy.Eager,
 })
-export class ProductsAdminPage {}
+export class ProductsAdminPage {
+
+  private productService = inject(ProductService);
+  public paginationService = inject(PaginationService);
+
+  productsResource = rxResource({
+    params: () => ({page: this.paginationService.currentPage() - 1}),
+    stream: ({params}) => {
+      return this.productService.getProducts({
+        offset: params.page * 9
+      })
+    }
+  })
+}
