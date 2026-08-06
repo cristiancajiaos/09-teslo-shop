f22fc359755af9a3abdf0dcccf29073d091c12d2
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-06 17:00:05 -0400

    En vista ProductsAdminPage se agrega selector para mostrar X elementos por página; Ajustes sobre Layout de AdminDashboard

--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { ProductTable } from '../../components/product-table/product-table';
 import { ProductService } from '@products/services/product-service';
 import { PaginationService } from '@shared/components/pagination/pagination-service';
@@ -17,12 +17,23 @@ export class ProductsAdminPage {
   private productService = inject(ProductService);
   public paginationService = inject(PaginationService);
 
+  productsPerPage = signal(10);
+
   productsResource = rxResource({
-    params: () => ({page: this.paginationService.currentPage() - 1}),
+    params: () => ({
+      page: this.paginationService.currentPage() - 1,
+      limit: this.productsPerPage()
+    }),
     stream: ({params}) => {
       return this.productService.getProducts({
-        offset: params.page * 9
+        offset: params.page * 9,
+        limit: params.limit
       })
     }
   })
+
+  changeProductsPerPage(value: string): void {
+    this.productsPerPage.set(parseInt(value));
+    this.productsResource.reload();
+  }
 }
