e3514dba635c3a7443d5c744e4d41e5679eb4922
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-08-07 10:37:32 -0400

    En la vista ProductDetails se preparan los tags para enviarse con el formato deseado para enviar al servicio

--- src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
@@ -57,6 +57,19 @@ export class ProductDetails implements OnInit {
 
   onSubmit(): void {
     const isValid = this.productForm.valid;
-    console.log(this.productForm.value, {isValid});
+    this.productForm.markAllAsTouched();
+
+    if (!isValid) {
+      return;
+    }
+
+    const formValue = this.productForm.value;
+
+    const productLike: Partial<Product> = {
+      ...(formValue as any),
+      tags: formValue.tags?.toLowerCase().split(',').map(tag => tag.trim()) ?? []
+    }
+
+    console.log({productLike});
   }
 }
